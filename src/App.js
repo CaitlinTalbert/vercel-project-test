5bd1860058c92874e365b742736644b36d23e7b8
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-01-27 15:08:05 -0600

    styling done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-
-import './App.css';
+import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      
-       <h1>Caitlin's Website</h1>
-     
-      <button>click to give me money here thanks</button>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
     </div>
   );
 }
